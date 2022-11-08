efeeb4a13d107bed050fac997ba62537fcc779ac
Paulo Endoh <paulo.endoh@gmail.com>
2022-11-08 13:42:49 -0300

    feat/cl9szfxqc202360m2b5mvw775s - show saved items in home

--- src/screens/HomeNavigationScreens/HomeScreen/HomeRatingItem/HomeRatingItem.tsx
+++ src/screens/HomeNavigationScreens/HomeScreen/HomeRatingItem/HomeRatingItem.tsx
@@ -1,10 +1,14 @@
+import { MaterialCommunityIcons } from "@expo/vector-icons"
 import { DateTime } from "luxon"
 import { HStack, Image, Pressable, Text, useTheme, VStack } from "native-base"
 import React, { useMemo } from "react"
 import UserProfilePicture from "../../../../components/UserProfilePicture/UserProfilePicture"
+import { useMyInterestsQuery } from "../../../../hooks/react-query/interest/useMyInterestsQuery"
+import { useMyRatingsQuery } from "../../../../hooks/react-query/rating/useMyRatingsQuery"
 import useAuthStore from "../../../../hooks/zustand/useAuthStore"
 import { RatingDto } from "../../../../types/domain/rating/RatingDto"
 import { getImageUrlOrDefaultUrl } from "../../../../utils/getImageUrlOrDefaultUrl"
+import HStackVCenter from "../../../_common/flexboxes/HStackVCenter"
 
 interface Props {
   rating: RatingDto
@@ -20,6 +24,18 @@ const HomeRatingItem = ({ rating, ...props }: Props) => {
   }, [rating.createdAt])
 
   const authUser = useAuthStore((s) => s.authUser)
+
+  const { data: myRatings } = useMyRatingsQuery()
+  const { data: myInterests } = useMyInterestsQuery()
+
+  const itemIsInMyList = useMemo(() => {
+    const rated =
+      myRatings?.find((r) => r.imdbItemId === rating.imdbItemId) || null
+    const interested =
+      myInterests?.find((r) => r.imdbItemId === rating.imdbItemId) || null
+    return Boolean(rated || interested)
+  }, [myRatings, myInterests, rating.imdbItemId])
+
   return (
     <Pressable onPress={() => props.onPress()}>
       <HStack key={rating.id} justifyContent="space-between">
@@ -42,11 +58,26 @@ const HomeRatingItem = ({ rating, ...props }: Props) => {
                 {rating.ratingValue}
               </Text>
             </Text>
-            <Text>
-              <Text>{rating.imdbItem?.title}</Text>{" "}
+            <Text numberOfLines={1}>
+              {rating.imdbItem?.title}{" "}
               {rating.imdbItem?.year && `(${rating.imdbItem?.year})`}
             </Text>
             <Text fontSize="xs">{timeAgo}</Text>
+
+            <HStackVCenter mt={1}>
+              {itemIsInMyList && (
+                <HStackVCenter space={1}>
+                  <MaterialCommunityIcons
+                    name="bookmark-box-multiple"
+                    size={16}
+                    color={theme.colors.secondary[500]}
+                  />
+                  <Text style={{ color: theme.colors.secondary[500] }}>
+                    Saved
+                  </Text>
+                </HStackVCenter>
+              )}
+            </HStackVCenter>
           </VStack>
         </HStack>
         <HStack width={100}>
