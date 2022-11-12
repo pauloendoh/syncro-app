2976e032573ae980b1abd9124e78a79b853b97f1
Paulo Endoh <paulo.endoh@gmail.com>
2022-11-12 20:41:11 -0300

    fix/cla8y0kcr59690mtnjdgcax9h - close toasts

--- src/components/toasts/useMyToast.tsx
+++ src/components/toasts/useMyToast.tsx
@@ -1,6 +1,6 @@
-import { Text, useToast } from "native-base"
+import { Ionicons } from "@expo/vector-icons"
+import { HStack, IconButton, Text, useToast } from "native-base"
 import React, { ReactNode } from "react"
-import HStackVCenter from "../../screens/_common/flexboxes/HStackVCenter"
 
 export const useMyToast = () => {
   const toast = useToast()
@@ -8,24 +8,26 @@ export const useMyToast = () => {
   const showSuccessToast = (children: ReactNode) =>
     toast.show({
       render: () => (
-        <HStackVCenter
-          py={2}
-          rounded="sm"
-          bgColor="success.500"
-          px={4}
-          space={2}
-        >
+        <HStack py={2} rounded="sm" bgColor="success.500" px={4}>
           <Text textAlign="center">{children}</Text>
-        </HStackVCenter>
+
+          <IconButton size="xs" onPress={() => toast.closeAll()}>
+            <Ionicons name="md-close-outline" size={12} color={"white"} />
+          </IconButton>
+        </HStack>
       ),
     })
 
   const showErrorToast = (children: ReactNode) =>
     toast.show({
       render: () => (
-        <HStackVCenter py={2} rounded="sm" bgColor="error.500" px={4}>
+        <HStack py={2} rounded="sm" bgColor="error.500" px={4}>
           <Text textAlign="center">{children}</Text>
-        </HStackVCenter>
+
+          <IconButton size="xs" onPress={() => toast.closeAll()}>
+            <Ionicons name="md-close-outline" size={12} color={"white"} />
+          </IconButton>
+        </HStack>
       ),
     })
 
