80e0f63e3fef3e8ea8c25b6d67e1a2dd8c6e8811
Paulo Endoh <paulo.endoh@gmail.com>
2022-11-22 18:16:21 -0300

    feat/clalig41d33630m0bnjtvgie5 - select instead of input

--- src/screens/ProfileNavigationScreens/UserItemsScreen/UserItem/CustomPositionSection/CustomPositionSection.tsx
+++ src/screens/ProfileNavigationScreens/UserItemsScreen/UserItem/CustomPositionSection/CustomPositionSection.tsx
@@ -1,9 +1,10 @@
 import { Select, Spinner, Text, VStack } from "native-base"
-import React, { useEffect, useMemo, useState } from "react"
+import React, { useMemo } from "react"
 import useChangeCustomPositionMutation from "../../../../../hooks/react-query/custom-position/useChangeCustomPositionMutation"
 import { useCustomPositionsQuery } from "../../../../../hooks/react-query/custom-position/useCustomPositionsQuery"
 import { useUserItemsQuery } from "../../../../../hooks/react-query/user/useUserItemsQuery"
 import useAuthStore from "../../../../../hooks/zustand/useAuthStore"
+import { buildCustomPositionDto } from "../../../../../types/domain/custom-position/CustomPositionDto"
 import { SyncroItemType } from "../../../../../types/domain/SyncroItemType"
 
 interface Props {
@@ -18,51 +19,42 @@ const CustomPositionSection = (props: Props) => {
   const authUser = useAuthStore((s) => s.authUser)
   const { data: userItems } = useUserItemsQuery(authUser!.id, props.itemType)
 
-  const itemsCount = useMemo(() => {
-    if (!userItems) return 0
-    return userItems.length
-  }, [userItems, authUser])
-
-  const currentPosition = useMemo(() => {
-    if (!customPositions) return undefined
-
-    const customPosition = customPositions.find(
-      (p) => p.imdbItemId === props.itemId
-    )
-    return customPosition
-  }, [customPositions, props.itemId])
+  const currentPosition = useMemo(
+    () => customPositions?.find((p) => p.imdbItemId === props.itemId),
+    [customPositions, props.itemId]
+  )
 
   const currentPositionLabel = useMemo(() => {
     if (!currentPosition) return "?"
 
-    return currentPosition?.position.toString() || "?"
+    return currentPosition.position.toString()
   }, [currentPosition])
-
-  const [input, setInput] = useState(currentPositionLabel)
-
-  // after saving
-  useEffect(() => {
-    setInput(currentPositionLabel)
-  }, [currentPositionLabel])
 
   const { mutate: submitChangePosition } = useChangeCustomPositionMutation()
 
   const handleSubmit = (newPosition: string) => {
-    if (!currentPosition) return
-    submitChangePosition(
-      {
+    if (!currentPosition) {
+      submitChangePosition({
         itemType: props.itemType,
         dto: {
-          ...currentPosition,
-          position: Number(newPosition),
+          ...buildCustomPositionDto({
+            imdbItemId: props.itemId,
+            userId: authUser!.id,
+            position: Number(newPosition),
+          }),
         },
+      })
+
+      return
+    }
+
+    submitChangePosition({
+      itemType: props.itemType,
+      dto: {
+        ...currentPosition,
+        position: Number(newPosition),
       },
-      {
-        onSuccess: () => {
-          setInput(newPosition)
-        },
-      }
-    )
+    })
   }
 
   const options = useMemo(() => userItems?.map((u, index) => index + 1) || [], [
@@ -75,7 +67,7 @@ const CustomPositionSection = (props: Props) => {
     <VStack>
       <Text>Position</Text>
       <Select
-        selectedValue={input}
+        selectedValue={currentPositionLabel}
         width="72px"
         _actionSheetBody={{
           size: "xs",
@@ -90,29 +82,6 @@ const CustomPositionSection = (props: Props) => {
           />
         ))}
       </Select>
-      {/* <Input
-        width="40px"
-        boxSize={"0.5"}
-        onChangeText={(text) => {
-          const number = parseInt(text)
-          if (isNaN(number)) {
-            setInput("")
-            return
-          }
-
-          if (number > itemsCount) {
-            setInput(itemsCount.toString())
-            return
-          }
-
-          setInput(number.toString())
-        }}
-        value={input}
-        onBlur={() => setInput(currentPositionLabel)}
-        onSubmitEditing={handleSubmit}
-        type="text"
-        keyboardType="numeric"
-      /> */}
     </VStack>
   )
 }
