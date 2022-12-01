9dde0674ea6f00971a95f921dcd60e1056c6f88b
Paulo Endoh <paulo.endoh@gmail.com>
2022-12-01 11:48:51 -0300

    feat/clb23h5aj126940mt55nq3yh5q - encrypt

--- decrypt_secret.sh
+++ decrypt_secret.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-# Decrypt the file
-mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$GPG_JSON_KEY" \
